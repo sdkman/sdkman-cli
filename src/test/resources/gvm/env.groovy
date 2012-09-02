bdeffbe14df6147b3025d9c34521cef8fd737d8b
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-09-02 14:02:23 +0100

    Add before hook.

--- src/test/resources/gvm/env.groovy
+++ src/test/resources/gvm/env.groovy
@@ -1,8 +1,11 @@
-this.metaClass.mixin(cucumber.runtime.groovy.Hooks)
+import static cucumber.runtime.groovy.Hooks.*
+import static gvm.VertxUtils.*
 
 home = System.getProperty('user.home')
 gvmDir = new File("${home}/.gvm")    
 testMarker = new File("${home}/.gvmtest")
+
+server = null
 
 Before(){
 	cleanUp()
@@ -17,3 +20,11 @@ After(){
 private cleanUp(){
 	if(gvmDir.directory) assert gvmDir.deleteDir()
 }
+
+Before("@webservice"){
+	def server = startServer()
+}
+
+//After("@webservice"){
+//	stopServer(server)
+//}
