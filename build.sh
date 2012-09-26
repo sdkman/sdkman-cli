6b0747e510981be5264682c0ef9c6b7111d12957
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-09-26 21:29:45 +0100

    Introduce Java Home to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home"
 export GVM_SERVICE="http://localhost:8080"
 export GVM_DIR="/tmp/gvm"
 ./gradlew -i clean test
