af4a8c7ca7bde2ed8149d0045c3b636141f3c236
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-10-22 15:48:03 +0100

    Bump version to 0.8 in stub.

--- src/test/groovy/gvm/VertxUtils.groovy
+++ src/test/groovy/gvm/VertxUtils.groovy
@@ -10,7 +10,7 @@ class VertxUtils {
 	final static candidates = [grails:grails]
 	final static defaults = [grails:'2.1.0']
 
-	static final gvmVersion = '0.7'
+	static final gvmVersion = '0.8'
 	static final vertxVersion = '1.2.3.final'
 
 
