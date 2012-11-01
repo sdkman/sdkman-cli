c6e5035837bca99fcf0873aabf55f94ea9a7eeee
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-11-01 17:08:48 +0000

    Invoke gradle with daemon flag.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home"
 export GVM_SERVICE="http://localhost:8080"
 export GVM_DIR="/tmp/gvm"
-./gradlew -i clean test
+./gradlew --daemon -i clean test
