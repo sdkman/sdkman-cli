d435adff5ff4cb4594ea573ff4179a59c3c56ebc
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-11-17 15:19:09 +0000

    Update selfupdate.sh to handle candidate folder creation.

--- srv/scripts/selfupdate.sh
+++ srv/scripts/selfupdate.sh
@@ -5,6 +5,11 @@ echo "Updating gvm scripts..."
 TMP_ZIP="/tmp/res.zip"
 BIN_FOLDER="$HOME/.gvm/bin"
 
+mkdir -p "$HOME/.gvm/groovy"
+mkdir -p "$HOME/.gvm/grails"
+mkdir -p "$HOME/.gvm/griffon"
+mkdir -p "$HOME/.gvm/vert.x"
+
 mkdir -p "$BIN_FOLDER"
 curl -s "$GVM_SERVICE/res?platform=$PLATFORM" > "$TMP_ZIP"
 unzip -qo "$TMP_ZIP" -d "$BIN_FOLDER"
