53e208694b4054dd077de000525b1a98fcaf7a3a
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-11-17 13:54:11 +0000

    Add vert.x home to gvm initialisation script.

--- srv/scripts/gvm-init.sh
+++ srv/scripts/gvm-init.sh
@@ -8,5 +8,6 @@ GROOVY_HOME="$HOME/.gvm/groovy/current"
 GRAILS_HOME="$HOME/.gvm/grails/current"
 GRIFFON_HOME="$HOME/.gvm/griffon/current"
 GRADLE_HOME="$HOME/.gvm/gradle/current"
+VERTX_HOME="$HOME/.gvm/vert.x/current"
 
-export PATH="$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$PATH"
+export PATH="$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$VERTX_HOME/bin:$PATH"
