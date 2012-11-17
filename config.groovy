0beb7362ed4d95f871b369731925cae3863259c4
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-11-17 11:10:56 +0000

    Introduce environments to Gradle build.

---
+++ config.groovy
@@ -0,0 +1,14 @@
+environments {
+	dev {
+		gvmVersion = '0.8.2'
+		gvmService = 'http://localhost:8080'
+	}
+	test {
+		gvmVersion = '0.8.2'
+		gvmService = 'http://test.gvmtool.net'
+	}
+	prod {
+		gvmVersion = '0.8.2'
+		gvmService = 'http://get.gvmtool.net'
+	}
+}
