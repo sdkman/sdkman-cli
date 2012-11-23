a8fd5cac5b830cd0ea026567fc0de53c82c97e86
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-11-23 14:01:16 +0000

    Remove dependency on zip command. Use unzip -T instead.

--- src/main/resources/scripts/install.sh
+++ src/main/resources/scripts/install.sh
@@ -72,18 +72,6 @@ if [ ! -f "$JAVA_HOME/bin/java" ]; then
 	exit 0	
 fi
 
-echo "Looking for zip..."
-if [ -z $(which zip) ]; then
-	echo "Not found."
-	echo "======================================================================================================"
-	echo " Please install zip on your system using your favourite package manager."
-	echo ""
-	echo " Restart after installing zip."
-	echo "======================================================================================================"
-	echo ""
-	exit 0
-fi
-
 echo "Looking for unzip..."
 if [ -z $(which unzip) ]; then
 	echo "Not found."
