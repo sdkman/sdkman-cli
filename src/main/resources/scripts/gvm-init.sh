548c671bcf110683a9c1232d2d827f8b97887fa6
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-11-26 16:42:39 -0600

    Added scripts for dynamically determining grails version

--- src/main/resources/scripts/gvm-init.sh
+++ src/main/resources/scripts/gvm-init.sh
@@ -2,12 +2,10 @@
 
 export GVM_SERVICE="@GVM_SERVICE@"
 
-PATH="$HOME/.gvm/bin:$PATH"
-
 GROOVY_HOME="$HOME/.gvm/groovy/current"
 GRAILS_HOME="$HOME/.gvm/grails/current"
 GRIFFON_HOME="$HOME/.gvm/griffon/current"
 GRADLE_HOME="$HOME/.gvm/gradle/current"
 VERTX_HOME="$HOME/.gvm/vert.x/current"
 
-export PATH="$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$VERTX_HOME/bin:$PATH"
+export PATH="$HOME/.gvm/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$VERTX_HOME/bin:$PATH"
