97042429f3f7a909ef263e1a4c18c64fdbbda0bc
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-11-26 22:16:07 -0600

    Moved grails scripts to their own directory and added flag for autodetect

--- src/main/resources/scripts/gvm-init.sh
+++ src/main/resources/scripts/gvm-init.sh
@@ -9,3 +9,8 @@ GRADLE_HOME="$HOME/.gvm/gradle/current"
 VERTX_HOME="$HOME/.gvm/vert.x/current"
 
 export PATH="$HOME/.gvm/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$VERTX_HOME/bin:$PATH"
+if [ "$GVM_GRAILS_AUTODETECT" -eq "1" ]; then
+    export PATH="$HOME/.gvm/bin/grails:$PATH"
+fi
+
+
