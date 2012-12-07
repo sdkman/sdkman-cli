5e28fb92824d72c469fbc43906adc543e2f19755
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-12-07 11:36:26 +0000

    Reapply code formatting on scripts.

--- src/main/resources/scripts/gvm-init.sh
+++ src/main/resources/scripts/gvm-init.sh
@@ -16,13 +16,12 @@
 #
 
 export GVM_SERVICE="@GVM_SERVICE@"
-export GVM_DIR="$HOME/.gvm"
 
-if [ -z "$GVM_DIR" ]; then
+if [ -z "${GVM_DIR}" ]; then
 	export GVM_DIR="$HOME/.gvm"
 fi
 
-PATH="$GVM_DIR/bin:$GVM_DIR/ext:$PATH"
+PATH="${GVM_DIR}/bin:${GVM_DIR}/ext:$PATH"
 
 GROOVY_HOME="${GVM_DIR}/groovy/current"
 GRAILS_HOME="${GVM_DIR}/grails/current"
@@ -30,16 +29,16 @@ GRIFFON_HOME="${GVM_DIR}/griffon/current"
 GRADLE_HOME="${GVM_DIR}/gradle/current"
 VERTX_HOME="${GVM_DIR}/vert.x/current"
 
-export PATH="$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRIFFON_HOME/bin:$GRADLE_HOME/bin:$VERTX_HOME/bin:$PATH"
+export PATH="${GROOVY_HOME}/bin:${GRAILS_HOME}/bin:${GRIFFON_HOME}/bin:${GRADLE_HOME}/bin:${VERTX_HOME}/bin:$PATH"
 
 # Source the main `gvm` script.
-source "$GVM_DIR/bin/gvm"
+source "${GVM_DIR}/bin/gvm"
 
 # Source any extension files whose names begin with 'sourced-'
-if [ -d "$GVM_DIR/ext" ]; then
-    for f in $GVM_DIR/ext/sourced-*; do
-        if [ -r $f ]; then
-            source $f
+if [ -d "${GVM_DIR}/ext" ]; then
+    for f in ${GVM_DIR}/ext/sourced-*; do
+        if [ -r ${f} ]; then
+            source ${f}
         fi
     done
     unset f
