d0d39f6798bc6f618969c42e3f80f10d80cde29e
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-12-12 16:20:27 +0000

    Updates grails versions 1.2.0 -> 1.3.4 to point to codehaus.

---
+++ db/0911_grails_versions.js
@@ -0,0 +1,13 @@
+db.versions.update({candidate:"grails", version:"1.2.0"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.0.zip"}});
+db.versions.update({candidate:"grails", version:"1.2.1"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.1.zip"}});
+db.versions.update({candidate:"grails", version:"1.2.2"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.2.zip"}});
+db.versions.update({candidate:"grails", version:"1.2.3"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.3.zip"}});
+db.versions.update({candidate:"grails", version:"1.2.4"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.4.zip"}});
+db.versions.update({candidate:"grails", version:"1.2.5"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.2.5.zip"}});
+
+db.versions.update({candidate:"grails", version:"1.3.0"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.3.0.zip"}});
+db.versions.update({candidate:"grails", version:"1.3.1"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.3.1.zip"}});
+db.versions.update({candidate:"grails", version:"1.3.2"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.3.2.zip"}});
+db.versions.update({candidate:"grails", version:"1.3.3"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.3.3.zip"}});
+db.versions.update({candidate:"grails", version:"1.3.4"}, { $set: {url:"http://dist.codehaus.org/grails/grails-1.3.4.zip"}});
+
