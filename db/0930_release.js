cc35065299e7d7022c92053a1f37056e6ff9aba2
Marco Vermeulen <vermeulen.mp@gmail.com>
2012-12-27 15:58:05 +0000

    Database release script for 0.9.3

---
+++ db/0930_release.js
@@ -0,0 +1,7 @@
+db.broadcast.remove();
+db.broadcast.insert({ "_id": 1, "text": "GVM version 0.9.3                            " });
+db.broadcast.insert({ "_id": 2, "text": "In this release:                             " });
+db.broadcast.insert({ "_id": 3, "text": " * GVM switches to a modular architecture.   " });
+db.broadcast.insert({ "_id": 7, "text": "                                             " });
+db.broadcast.insert({ "_id": 8, "text": "Report any issues at:                        " });
+db.broadcast.insert({ "_id": 9, "text": "  https://github.com/gvmtool/gvm/issues      " });
