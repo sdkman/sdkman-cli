eebd3899d1523a7630ccc0f3f5d58f264965c93c
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-01-02 14:51:35 +0000

    Updates to 0.9.4 mongodb script.

--- db/0940_release.js
+++ db/0940_release.js
@@ -1,11 +1,20 @@
+//broadcast
 db.broadcast.remove();
-db.broadcast.insert({ "_id": 1, "text": "GVM version 0.9.4                            " });
-db.broadcast.insert({ "_id": 2, "text": "In this release:                             " });
-db.broadcast.insert({ "_id": 3, "text": " * vert.x renamed to vertx                   " });
-db.broadcast.insert({ "_id": 4, "text": "                                             " });
-db.broadcast.insert({ "_id": 5, "text": "Report any issues at:                        " });
-db.broadcast.insert({ "_id": 6, "text": "  https://github.com/gvmtool/gvm/issues      " });
+db.broadcast.insert({ "_id": 1, "text": "GVM version 0.9.4                              " });
+db.broadcast.insert({ "_id": 2, "text": "In this release:                               " });
+db.broadcast.insert({ "_id": 3, "text": " * vert.x renamed to vertx                     " });
+db.broadcast.insert({ "_id": 4, "text": " * extension modules rework (thanks Jeff Beck!)" });
+db.broadcast.insert({ "_id": 5, "text": " * robots.txt disallow on web service          " });
+db.broadcast.insert({ "_id": 6, "text": "                                               " });
+db.broadcast.insert({ "_id": 7, "text": "Report any issues at:                          " });
+db.broadcast.insert({ "_id": 8, "text": "  https://github.com/gvmtool/gvm/issues        " });
 
-//rename vert.x to vertx
-db.candidates.update({candidate:"vert.x"}, {$set:{candidate:"vertx"}});
-db.versions.update({candidate:"vert.x"}, {$set:{candidate:"vertx"}}, {multi:true});
+//insert vertx candidate and versions
+db.candidates.insert({candidate:"vertx", default: "1.3.0.final"});
+db.versions.insert({candidate:"vertx", version:"1.1.0.final", url:"https://s3.amazonaws.com/vert.x/vert.x-1.1.0.final.zip"});
+db.versions.insert({candidate:"vertx", version:"1.2.0.final", url:"https://s3.amazonaws.com/vert.x/vert.x-1.2.0.final.zip"});
+db.versions.insert({candidate:"vertx", version:"1.2.1.final", url:"https://s3.amazonaws.com/vert.x/vert.x-1.2.1.final.zip"});
+db.versions.insert({candidate:"vertx", version:"1.2.2.final", url:"https://s3.amazonaws.com/vert.x/vert.x-1.2.2.final.zip"});
+db.versions.insert({candidate:"vertx", version:"1.2.3.final", url:"http://vertx.io/downloads/vert.x-1.2.3.final.zip"});
+db.versions.insert({candidate:"vertx", version:"1.3.0.final", url:"http://vertx.io/downloads/vert.x-1.3.0.final.zip"});
+
