9c043bd11c918f1d5c225cfca95d60bb07f23a80
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-01-12 22:10:53 +0000

    Rework installation step definitions.

--- src/test/resources/gvm/installation_steps.groovy
+++ src/test/resources/gvm/installation_steps.groovy
@@ -2,6 +2,7 @@ package gvm
 
 import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.nio.file.Path
 
 import static cucumber.runtime.groovy.EN.*
 
@@ -23,35 +24,62 @@ Given(~'^the candidate "([^"]*)" version "([^"]*)" is not installed$') { String 
 }
 
 When(~'^the candidate "([^"]*)" version "([^"]*)" is already installed and default$') { String candidate, String version ->
-    def candidateVersion = setupCandidateVersion(candidate, version)
-
+    def candidateVersion = prepareCandidateFolder("$gvmDir", candidate, version)
     def currentLink = FileSystems.default.getPath("$gvmDir/$candidate/current")
     Files.createSymbolicLink currentLink, candidateVersion
 }
 
 When(~'^the candidate "([^"]*)" version "([^"]*)" is already installed but not default$') { String candidate, String version ->
-    setupCandidateVersion candidate, version
+    prepareCandidateFolder "$gvmDir", candidate, version
 }
-
-private setupCandidateVersion(String candidate, String version) {
-    def fileSystem = FileSystems.default
-
-    def versionLocation = "$gvmDir/$candidate/$version"
-    def versionFolder = fileSystem.getPath(versionLocation)
-    Files.createDirectories versionFolder
-
-    def binFolder = fileSystem.getPath("$versionLocation/bin")
-    Files.createDirectories binFolder
-
-    def candidateFile = new File("$versionLocation/bin/$candidate")
-    candidateFile.write "echo ${candidate.capitalize()} Version: ${version}"
-    candidateFile.executable = true
-    versionFolder
-}
-
 
 Given(~'^I do not have a "([^"]*)" candidate installed$') { String candidate ->
 	def file = new File("${gvmDir}/${candidate}")
 	file.delete()
 	assert ! file.exists()
+}
+
+Given(~'^the candidate "([^"]*)" version "([^"]*)" does not exist$') { String candidate, String version ->
+    def versions = new URL("${serviceUrlEnv}/candidates/${candidate}").text
+    assert ! versions.contains(version)
+}
+
+And(~'^I have a local candidate "([^"]*)" version "([^"]*)" at "([^"]*)"$') { String candidate, String version, String directory ->
+    prepareLocalCandidateFolder directory, candidate, version
+}
+
+And(~'^the candidate "([^"]*)" version "([^"]*)" is linked to "([^"]*)"$') { String candidate, String version, String directory ->
+    def fileSystem = FileSystems.default
+
+    def versionLocation = "$gvmDir/$candidate/$version"
+    def versionFolder = fileSystem.getPath(versionLocation)
+
+    assert Files.isSymbolicLink(versionFolder)
+    assert Files.readSymbolicLink(versionFolder).toString() == directory
+}
+
+private prepareCandidateFolder(String baseDir, String candidate, String version) {
+    def directory = "$baseDir/$candidate/$version"
+    prepareCandidateBinFolder directory, candidate, version
+}
+
+private prepareLocalCandidateFolder(String baseDir, String candidate, String version){
+    prepareCandidateBinFolder baseDir, candidate, version
+}
+
+private prepareCandidateBinFolder(String folder, String candidate, String version) {
+    def fileSystem = FileSystems.default
+    def folderPath = fileSystem.getPath("$folder")
+
+    def binFolder = fileSystem.getPath("$folder/bin")
+    Files.createDirectories binFolder
+    prepareCandidateExecutable binFolder, candidate, version
+
+    folderPath
+}
+
+private prepareCandidateExecutable(Path binFolder, String candidate, String version) {
+    def candidateFile = new File("$binFolder/$candidate")
+    candidateFile.write "echo ${candidate.capitalize()} Version: ${version}"
+    candidateFile.executable = true
 }
