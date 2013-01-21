90b04751f35de4b8e9721f9d21a8dea44f291a41
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-01-21 13:52:21 +0000

    Upgrade version to 0.9.5, add mongo release script.

---
+++ db/0950_release.js
@@ -0,0 +1,9 @@
+//broadcast
+db.broadcast.remove();
+db.broadcast.insert({ "_id": 1, "text": "GVM version 0.9.5                              " });
+db.broadcast.insert({ "_id": 2, "text": "In this release:                               " });
+db.broadcast.insert({ "_id": 3, "text": " * local dev candidate installation            " });
+db.broadcast.insert({ "_id": 4, "text": " * various bug fixes                           " });
+db.broadcast.insert({ "_id": 5, "text": "                                               " });
+db.broadcast.insert({ "_id": 6, "text": "Report any issues at:                          " });
+db.broadcast.insert({ "_id": 7, "text": "  https://github.com/gvmtool/gvm/issues        " });
