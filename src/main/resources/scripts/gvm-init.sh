07a5fd416d00072799b4524bee0e2041487ce538
Dave Syer <dsyer@vmware.com>
2013-02-04 15:38:28 +0000

    Promote gvm-init.sh to be a first class function

--- src/main/resources/scripts/gvm-init.sh
+++ src/main/resources/scripts/gvm-init.sh
@@ -19,84 +19,97 @@
 export GVM_VERSION="@GVM_VERSION@"
 export GVM_PLATFORM=$(uname)
 
-# OS specific support (must be 'true' or 'false').
-cygwin=false;
-darwin=false;
-case "`uname`" in
-    CYGWIN*)
-        cygwin=true
-        ;;
+function __gvmtool_contains {
+    replaced=$(echo "$1" | sed -e s,"$2",,g)
+    [ "$replaced" != "$1" ]
+}
+
+function __gvmtool_init {
+
+    # OS specific support (must be 'true' or 'false').
+    cygwin=false;
+    darwin=false;
+    case "`uname`" in
+        CYGWIN*)
+            cygwin=true
+            ;;
         
-    Darwin*)
-        darwin=true
-        ;;
-esac
+        Darwin*)
+            darwin=true
+            ;;
+    esac
 
-GVM_SERVICE_DEFAULT="@GVM_SERVICE@"
-GVM_CANDIDATES_DEFAULT=("groovy" "grails" "griffon" "gradle" "vertx")
+    GVM_SERVICE_DEFAULT="@GVM_SERVICE@"
+    GVM_CANDIDATES_DEFAULT=("groovy" "grails" "griffon" "gradle" "vertx")
 
-if [ -z "${GVM_DIR}" ]; then
-	export GVM_DIR="$HOME/.gvm"
-fi
-mkdir -p $GVM_DIR/var/candidates
+    if [ -z "${GVM_DIR}" ]; then
+	    export GVM_DIR="$HOME/.gvm"
+    fi
+    mkdir -p $GVM_DIR/var/candidates
 
-if [ -z "${GVM_SERVICE}" ]; then
-	if [ -f "${GVM_DIR}/var/service" ]; then
-		GVM_SERVICE=$(cat "${GVM_DIR}/var/service")
-	else
-		GVM_SERVICE=${GVM_SERVICE_DEFAULT}
-	fi
-fi
-export GVM_SERVICE
-echo -n ${GVM_SERVICE} > "${GVM_DIR}/var/service"
+    if [ -z "${GVM_SERVICE}" ]; then
+	    if [ -f "${GVM_DIR}/var/service" ]; then
+		    GVM_SERVICE=$(cat "${GVM_DIR}/var/service")
+	    else
+		    GVM_SERVICE=${GVM_SERVICE_DEFAULT}
+	    fi
+    fi
+    export GVM_SERVICE
+    echo -n ${GVM_SERVICE} > "${GVM_DIR}/var/service"
 
-# check cached candidates first
-candidate_cache="${GVM_DIR}/var/candidates/$(echo ${GVM_SERVICE} | tr ':/' '_')"
-if [ -f "${candidate_cache}" -a "${*/--flush/}" == "${*}" ]; then
-	GVM_CANDIDATES=($(cat "${candidate_cache}"))
-else
-	GVM_CANDIDATES=($(curl -s "${GVM_SERVICE}/candidates" | sed -e 's/,//g'))
-	if [[ "${#GVM_CANDIDATES[@]}" == "0" ]]; then
-		GVM_CANDIDATES=(${GVM_CANDIDATES_DEFAULT[@]})
-	else
-		# only cache the candidates if derived from online service
-		echo -n ${GVM_CANDIDATES[@]} > "${candidate_cache}"
-	fi
-fi
-export GVM_CANDIDATES
+    # check cached candidates first
+    candidate_cache="${GVM_DIR}/var/candidates/$(echo ${GVM_SERVICE} | tr ':/' '_')"
+    if [ -f "${candidate_cache}" -a "${*/--flush/}" == "${*}" ]; then
+	    GVM_CANDIDATES=($(cat "${candidate_cache}"))
+    else
+	    GVM_CANDIDATES=($(curl -s "${GVM_SERVICE}/candidates" | sed -e 's/,//g'))
+	    if [[ "${#GVM_CANDIDATES[@]}" == "0" ]]; then
+		    GVM_CANDIDATES=(${GVM_CANDIDATES_DEFAULT[@]})
+	    else
+		    # only cache the candidates if derived from online service
+		    echo -n ${GVM_CANDIDATES[@]} > "${candidate_cache}"
+	    fi
+    fi
+    export GVM_CANDIDATES
 
-OFFLINE_BROADCAST=$( cat << EOF
+    OFFLINE_BROADCAST=$( cat << EOF
 ==== BROADCAST =============================================
 
 AEROPLANE MODE ENABLED! Some functionality is now disabled.
 
 ============================================================
 EOF
-)
+    )
 
-ONLINE_BROADCAST=$( cat << EOF
+    ONLINE_BROADCAST=$( cat << EOF
 ==== BROADCAST =============================================
 
 ONLINE MODE RE-ENABLED! All functionality now restored.
 
 ============================================================
 EOF
-)
+    )
 
-OFFLINE_MESSAGE="This command is not available in aeroplane mode."
-PATH="${GVM_DIR}/bin:${GVM_DIR}/ext:$PATH"
-
-# Source gvm module scripts.
-for f in $(find "${GVM_DIR}/src" -type f -name 'gvm-*'); do
-	source "${f}"
-done
-
-# Source extension files prefixed with 'gvm-' and found in the ext/ folder
-# Use this if extensions are written with the functional approach and want
-# to use functions in the main gvm script.
-for f in $(find "${GVM_DIR}/ext" -type f -name 'gvm-*'); do
-	if [ -r "${f}" ]; then
-		source "${f}"
+    OFFLINE_MESSAGE="This command is not available in aeroplane mode."
+    if ! __gvmtool_contains "$PATH" "$GVM_DIR"; then
+        PATH="${GVM_DIR}/bin:${GVM_DIR}/ext:$PATH"
     fi
-done
-unset f
+
+    # Source gvm module scripts (except this one).
+    for f in $(find "${GVM_DIR}/src" -type f -name 'gvm-*'); do
+	    if [ "${f##*/}" != "gvm-init.sh" ]; then source "${f}"; fi
+    done
+
+    # Source extension files prefixed with 'gvm-' and found in the ext/ folder
+    # Use this if extensions are written with the functional approach and want
+    # to use functions in the main gvm script.
+    for f in $(find "${GVM_DIR}/ext" -type f -name 'gvm-*'); do
+	    if [ -r "${f}" ]; then
+		    source "${f}"
+        fi
+    done
+    unset f
+}
+
+__gvmtool_init
+
