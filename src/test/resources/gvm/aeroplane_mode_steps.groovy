7289d253a3f92775b8295515117038928b170b0e
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-03-28 21:48:53 +0000

    Rename base directory to gvm base environment variable in test environment.

--- src/test/resources/gvm/aeroplane_mode_steps.groovy
+++ src/test/resources/gvm/aeroplane_mode_steps.groovy
@@ -3,14 +3,14 @@ package gvm
 import static cucumber.api.groovy.EN.*
 
 Given(~'^the internet is not reachable$') {->
-    bash = new BashEnv(baseDir, [GVM_DIR: gvmDirEnv, GVM_SERVICE: "http://localhost:0"])
+    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: "http://localhost:0"])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
 
 
 And(~'^the internet is reachable$') {->
-    bash = new BashEnv(baseDir, [GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv])
+    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
