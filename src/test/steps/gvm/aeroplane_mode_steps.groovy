25b893e69669b8da813c0313b84432c6155c75db
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-04-17 07:13:09 +0100

    Initialise fake JAVA_HOME in tests.

--- src/test/steps/gvm/aeroplane_mode_steps.groovy
+++ src/test/steps/gvm/aeroplane_mode_steps.groovy
@@ -2,15 +2,18 @@ package gvm
 
 import static cucumber.api.groovy.EN.*
 
+final SERVICE_DOWN = "http://localhost:0"
+final FAKE_JDK_PATH = "/path/to/my/openjdk"
+
 Given(~'^the internet is not reachable$') {->
-    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: "http://localhost:0"])
+    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: SERVICE_DOWN, JAVA_HOME: FAKE_JDK_PATH])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
 
 
 And(~'^the internet is reachable$') {->
-    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv])
+    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv, JAVA_HOME: FAKE_JDK_PATH])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
