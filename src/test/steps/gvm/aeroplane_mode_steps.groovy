cdda8245161f1a0f2105b6dff003bd730ea4eec3
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-05-03 10:22:15 +0100

    Explicitly set GVM_INIT to false in test setup.

--- src/test/steps/gvm/aeroplane_mode_steps.groovy
+++ src/test/steps/gvm/aeroplane_mode_steps.groovy
@@ -6,14 +6,14 @@ final SERVICE_DOWN = "http://localhost:0"
 final FAKE_JDK_PATH = "/path/to/my/openjdk"
 
 Given(~'^the internet is not reachable$') {->
-    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: SERVICE_DOWN, JAVA_HOME: FAKE_JDK_PATH])
+    bash = new BashEnv(gvmBaseEnv, [GVM_INIT: 'false', GVM_DIR: gvmDirEnv, GVM_SERVICE: SERVICE_DOWN, JAVA_HOME: FAKE_JDK_PATH])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
 
 
 And(~'^the internet is reachable$') {->
-    bash = new BashEnv(gvmBaseEnv, [GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv, JAVA_HOME: FAKE_JDK_PATH])
+    bash = new BashEnv(gvmBaseEnv, [GVM_INIT: 'false', GVM_DIR: gvmDirEnv, GVM_SERVICE: serviceUrlEnv, JAVA_HOME: FAKE_JDK_PATH])
     bash.start()
     bash.execute("source $binDir/gvm-init.sh")
 }
