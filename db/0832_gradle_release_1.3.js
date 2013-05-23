963341ca5c5864100e9aaf69f1fc928a29570e35
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-05-23 13:15:13 +0100

    Remove obsolete js folder.

--- db/0832_gradle_release_1.3.js
+++
@@ -1,2 +0,0 @@
-db.versions.insert({candidate:"gradle", version:"1.3", url:"http://services.gradle.org/distributions/gradle-1.3-all.zip"})
-db.candidates.update({candidate:"gradle"}, {$set:{default:"1.3"}})
--- db/0920_release.js
+++
@@ -1,7 +0,0 @@
-db.broadcast.remove();
-db.broadcast.insert({ "_id": 1, "text": "GVM version 0.9.2                            " });
-db.broadcast.insert({ "_id": 2, "text": "In this release:                             " });
-db.broadcast.insert({ "_id": 3, "text": " * Aeroplane mode!                           " });
-db.broadcast.insert({ "_id": 7, "text": "                                             " });
-db.broadcast.insert({ "_id": 8, "text": "Report any issues at:                        " });
-db.broadcast.insert({ "_id": 9, "text": "  https://github.com/gvmtool/gvm/issues      " });
