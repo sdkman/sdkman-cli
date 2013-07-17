e96dad51aee2d271a9daa6e4d4e029ca90147e32
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-07-17 18:45:59 +0100

    Add setversion script for ci.

---
+++ scripts/setversion.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+RELEASE=$(grep 'gvmVersion' config.groovy | sed 's_gvmVersion = __g' | tr -d "'")
+
+if [[ "$RELEASE" == "1.0.0-SNAPSHOT" ]]; then
+	RELEASE="1.0.0+build-$DRONE_BUILD_NUMBER"
+	sed -i "s/1.0.0-SNAPSHOT/$RELEASE/g" config.groovy
+fi
+
+echo "Release: $RELEASE"
+
+
+./gradlew -Penv="$DRONE_BRANCH"
+git add -f build
+git commit -m "Release: $RELEASE"
