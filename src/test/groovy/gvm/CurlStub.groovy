4367b267d9f053b6dc1e4e25bd8cc6e675aacf1b
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-07-21 15:05:14 -0500

    Reformat CurlStub.

--- src/test/groovy/gvm/CurlStub.groovy
+++ src/test/groovy/gvm/CurlStub.groovy
@@ -1,16 +1,16 @@
 package gvm
 
 class CurlStub {
-	
-	static prepareIn(String folder){
-		def file = new File("$folder/curl")
-		file.createNewFile()
-		file.write "#!/bin/bash\n"
-		file.executable = true
-		file
-	}	
 
-	void primeWith(String bash){
-		file.append bash
-	}
+    static prepareIn(String folder) {
+        def file = new File("$folder/curl")
+        file.createNewFile()
+        file.write "#!/bin/bash\n"
+        file.executable = true
+        file
+    }
+
+    void primeWith(String bash) {
+        file.append bash
+    }
 }
