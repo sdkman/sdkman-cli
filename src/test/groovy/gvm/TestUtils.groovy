97dd55a11f1450a518d1acd736bf2db40d81d67f
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-09-09 17:27:53 +0100

    Move default base dir back to /tmp folder, set as constant in TestUtils.

--- src/test/groovy/gvm/TestUtils.groovy
+++ src/test/groovy/gvm/TestUtils.groovy
@@ -2,9 +2,12 @@ package gvm
 
 
 class TestUtils {
+
+    static final DEFAULT_BASE_DIR = "/tmp/base"
+
     static File prepareBaseDir() {
         def counter = "${(Math.random() * 10000).toInteger()}".padLeft(4, "0")
-        def baseDir = "build/base/gvm-$counter" as File
+        def baseDir = "$DEFAULT_BASE_DIR/gvm-$counter" as File
         baseDir.mkdirs()
         baseDir
     }
