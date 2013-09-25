cfbb95867dde4a9ef8695491e9fa1568354357ad
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-09-25 16:25:27 +0200

    Fix code format of new script sourcing by @robertomoutinho. Also propagated to ext scripts.

--- src/main/bash/gvm-init.sh
+++ src/main/bash/gvm-init.sh
@@ -29,17 +29,15 @@ fi
 
 function gvm_source_modules {
 	# Source gvm module scripts.
-        for f in $(find "${GVM_DIR}/src" -type f -name 'gvm-*' -exec basename {} \;); do
-                source "${GVM_DIR}/src/${f}"
-        done
+    for f in $(find "${GVM_DIR}/src" -type f -name 'gvm-*' -exec basename {} \;); do
+        source "${GVM_DIR}/src/${f}"
+    done
 
 	# Source extension files prefixed with 'gvm-' and found in the ext/ folder
 	# Use this if extensions are written with the functional approach and want
 	# to use functions in the main gvm script.
-	for f in $(find "${GVM_DIR}/ext" -type f -name 'gvm-*'); do
-		if [ -r "${f}" ]; then
-			source "${f}"
-		fi
+	for f in $(find "${GVM_DIR}/ext" -type f -name 'gvm-*' -exec basename {} \;); do
+		source "${GVM_DIR}/ext/${f}"
 	done
 	unset f
 }
