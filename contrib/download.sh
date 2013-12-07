35ad11b4466d6ebf2f333582d3518c850fa98ba1
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-12-07 19:16:12 +0000

    Fixes download.sh: Move archive content to intermediary folder, remove duplicated folder structure.

--- contrib/download.sh
+++ contrib/download.sh
@@ -11,12 +11,19 @@ fi
 API="http://api.gvmtool.net"
 BASE_DIR="$HOME/gvm"
 ARCHIVE_DIR="${BASE_DIR}/archives"
+TMP_DIR="${BASE_DIR}/tmp"
 ARCHIVE="${ARCHIVE_DIR}/${CANDIDATE}-${VERSION}.zip"
 ARCHIVE_OK=$(unzip -t "${ARCHIVE}" | grep 'No errors detected in compressed data')
 DESTINATION_DIR="$BASE_DIR/$CANDIDATE-$VERSION"
 
+if [ -d "$TMP_DIR" ]; then
+    echo "Cleaning up temporary folder..."
+    rm -rf "$TMP_DIR"
+fi
+
 mkdir -p "$BASE_DIR"
 mkdir -p "$ARCHIVE_DIR"
+mkdir "$TMP_DIR"
 
 if [ -d "$DESTINATION_DIR" ]; then
     echo "Version $VERSION of $CANDIDATE already found, skipping installation."
@@ -31,4 +38,8 @@ else
 fi
 
 echo "Extracting archive to: $DESTINATION_DIR"
-unzip -oq "$ARCHIVE" -d "$DESTINATION_DIR"
+unzip -oq "$ARCHIVE" -d "$TMP_DIR"
+mkdir "$DESTINATION_DIR"
+mv "$TMP_DIR"/**/* "$DESTINATION_DIR"
+
+rm -rf "$TMP_DIR"
