511f5f3e9a9a26ea1cb941a7f2648465678f31e2
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-12-07 18:01:33 +0000

    Fixes download.sh: Check cached archive integrity.

--- contrib/download.sh
+++ contrib/download.sh
@@ -12,6 +12,7 @@ API="http://api.gvmtool.net"
 BASE_DIR="$HOME/gvm"
 ARCHIVE_DIR="${BASE_DIR}/archives"
 ARCHIVE="${ARCHIVE_DIR}/${CANDIDATE}-${VERSION}.zip"
+ARCHIVE_OK=$(unzip -t "${ARCHIVE}" | grep 'No errors detected in compressed data')
 DESTINATION_DIR="$BASE_DIR/$CANDIDATE-$VERSION"
 
 mkdir -p "$BASE_DIR"
@@ -22,11 +23,11 @@ if [ -d "$DESTINATION_DIR" ]; then
     exit 0
 fi
 
-if [ -f "$ARCHIVE" ]; then
-	echo "Archive found, using cached version..."
+if [[ -f "$ARCHIVE" && -n "$ARCHIVE_OK" ]]; then
+	echo "Archive found and verified, using cached version..."
 else
 	echo "Downloading archive..."
-	curl -# -L "${API}/download/${CANDIDATE}/${VERSION}?platform=$(uname)" >> "$ARCHIVE"
+	curl -# -L "${API}/download/${CANDIDATE}/${VERSION}?platform=$(uname)" > "$ARCHIVE"
 fi
 
 echo "Extracting archive to: $DESTINATION_DIR"
