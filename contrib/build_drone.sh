3be5522136a4f0ff00d0eff2bdd0fb2c4386045a
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-08-02 13:12:04 +0100

    Alter symantic versioning scheme.

--- contrib/build_drone.sh
+++ contrib/build_drone.sh
@@ -3,7 +3,7 @@
 RELEASE=$(grep 'gvmVersion' config.groovy | sed 's_gvmVersion = __g' | tr -d "'")
 
 if [[ "$RELEASE" == "1.0.0-SNAPSHOT" ]]; then
-	RELEASE="1.0.0+build-$DRONE_BUILD_NUMBER"
+	RELEASE="1.0.0-build-$DRONE_BUILD_NUMBER"
 	sed -i "s/1.0.0-SNAPSHOT/$RELEASE/g" config.groovy
 fi
 
