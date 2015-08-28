0b473503fed01533481a827dfeb40555962e94b3
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-08-28 13:08:57 +0100

    Issue #23 - Adding explicit SDKMAN_DIR export to bashrc snippet.
    Conflicts:
    	src/main/bash/install.sh

--- src/main/bash/install.sh
+++ src/main/bash/install.sh
@@ -42,6 +42,7 @@ sdkman_platform=$(uname)
 
 sdkman_init_snippet=$( cat << EOF
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$SDKMAN_DIR"
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
 EOF
 )
