2bd099d3241d68d13fa18f94ad9c0fe3d0b734d5
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-10-23 17:48:12 +0100

    IMPROVEMENT - Update CI build script and rename.

--- bin/build_ci.sh
+++ bin/build_ci.sh
@@ -3,12 +3,12 @@
 RELEASE=$(grep 'sdkmanVersion' config.groovy | sed 's_sdkmanVersion = __g' | tr -d "'")
 
 if [[ "$RELEASE" == "1.0.0-SNAPSHOT" ]]; then
-	RELEASE="1.0.0-build-$DRONE_BUILD_NUMBER"
+	RELEASE="1.0.0-build-$CI_BUILD_NUMBER"
 	sed -i "s/1.0.0-SNAPSHOT/$RELEASE/g" config.groovy
 fi
 
 echo "Release: $RELEASE"
 
-./gradlew -Penv="$DRONE_BRANCH"
+./gradlew -Penv="$CI_BRANCH"
 git add -f build
 git commit -m "Release: $RELEASE"
