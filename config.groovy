18f5b78e5d0dd54624e23f2ac26cf7b3690d59ad
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-03-28 10:36:28 +0100

    Introduce bintray publishing.

--- config.groovy
+++ config.groovy
@@ -1,19 +1,18 @@
-vertxVersion = '1.3.1.final'
-sdkmanVersion = '1.0.0-SNAPSHOT'
+sdkmanCliVersion = '1.0.0-SNAPSHOT'
 environments {
 	local {
 		sdkmanService = 'http://localhost:8080'
-        sdkmanBroadcastService = 'http://localhost:8080'
-        sdkmanBrokerService = 'http://localhost:8080'
+		sdkmanBroadcastService = 'http://localhost:8080'
+		sdkmanBrokerService = 'http://localhost:8080'
 	}
 	master {
 		sdkmanService = 'http://dev.sdkman.io'
-        sdkmanBroadcastService = 'http://cast-dev.sdkman.io'
-        sdkmanBrokerService = 'http://broker-dev.sdkman.io'
+		sdkmanBroadcastService = 'http://cast-dev.sdkman.io'
+		sdkmanBrokerService = 'http://broker-dev.sdkman.io'
 	}
 	production {
 		sdkmanService = 'http://api.sdkman.io'
-        sdkmanBroadcastService = 'http://cast.sdkman.io'
-        sdkmanBrokerService = 'http://broker.sdkman.io'
+		sdkmanBroadcastService = 'http://cast.sdkman.io'
+		sdkmanBrokerService = 'http://broker.sdkman.io'
 	}
 }
