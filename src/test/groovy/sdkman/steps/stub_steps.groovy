36788730b01586fa470d75d4c77055087fd889f6
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-11-04 18:11:28 +0000

    Cut validation over to new candidate validation endpoint.

--- src/test/groovy/sdkman/steps/stub_steps.groovy
+++ src/test/groovy/sdkman/steps/stub_steps.groovy
@@ -18,25 +18,25 @@ And(~'^an available selfupdate$') { ->
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is available for download$') { String candidate, String version ->
-    primeEndpointWithString("/candidates/${candidate}/${version}/${PLATFORM}", "valid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${PLATFORM}", "valid")
     primeDownloadFor(SERVICE_UP_URL, candidate, version, PLATFORM)
     primeEndpointWithString("/hooks/post/${candidate}/${version}/${PLATFORM}", 'mv $binary_input $zip_output') //bash command
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is not available for download$') { String candidate, String version ->
-    primeEndpointWithString("/candidates/${candidate}/${version}/${PLATFORM}", "invalid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${PLATFORM}", "invalid")
 }
 
 And(~/^the candidate "(.*?)" version "(.*?)" is available for download on "(.*?)"$/) { String candidate, String version, String platform ->
     String lowerCaseUname = UnixUtils.asUname(platform).toLowerCase()
-    primeEndpointWithString("/candidates/${candidate}/${version}/${lowerCaseUname}", "valid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${lowerCaseUname}", "valid")
     primeEndpointWithString("/hooks/post/${candidate}/${version}/${lowerCaseUname}", 'mv $binary_input $zip_output') //bash command
     primeDownloadFor(SERVICE_UP_URL, candidate, version, lowerCaseUname)
 }
 
 And(~/^the candidate "(.*?)" version "(.*?)" is not available for download on "(.*?)"$/) { String candidate, String version, String platform ->
     String lowerCaseUname = UnixUtils.asUname(platform).toLowerCase()
-    primeEndpointWithString("/candidates/${candidate}/${version}/${lowerCaseUname}", "invalid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${lowerCaseUname}", "invalid")
 }
 
 And(~'^a "([^"]*)" list view is available for consumption$') { String candidate ->
@@ -44,11 +44,11 @@ And(~'^a "([^"]*)" list view is available for consumption$') { String candidate 
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is a valid candidate version$') { String candidate, String version ->
-    primeEndpointWithString("/candidates/${candidate}/${version}/${PLATFORM}", "valid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${PLATFORM}", "valid")
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is not a valid candidate version$') { String candidate, String version ->
-    primeEndpointWithString("/candidates/${candidate}/${version}/${PLATFORM}", "invalid")
+    primeEndpointWithString("/candidates/validate/${candidate}/${version}/${PLATFORM}", "invalid")
 }
 
 And(~/^the candidate "(.*?)" has a version list available$/) { String candidate ->
