629054d8d195ca17ac1fa516c14bb9408ecfd268
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-03-25 16:11:37 +0000

    Consume new candidate list api.

--- src/main/bash/sdkman-list.sh
+++ src/main/bash/sdkman-list.sh
@@ -30,7 +30,7 @@ function __sdkman_list_candidates {
 	if [[ "$SDKMAN_AVAILABLE" == "false" ]]; then
 		__sdkman_echo_red "This command is not available while offline."
 	else
-		__sdkman_page echo "$(__sdkman_secure_curl "${SDKMAN_LEGACY_API}/candidates/list")"
+		__sdkman_page echo "$(__sdkman_secure_curl "${SDKMAN_CURRENT_API}/candidates/list")"
 	fi
 }
 
