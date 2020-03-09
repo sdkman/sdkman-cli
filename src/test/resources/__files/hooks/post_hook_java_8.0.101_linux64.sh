886461e3d98940c0c5fc47fb51b1f9a2a7cdfaef
Ross Goldberg <ross.goldberg@gmail.com>
2020-03-09 04:48:55 -0400

    spelling: requirment  -> requirement can not     -> cannot unselecting -> deselecting

--- src/test/resources/__files/hooks/post_hook_java_8.0.101_linux64.sh
+++ src/test/resources/__files/hooks/post_hook_java_8.0.101_linux64.sh
@@ -2,7 +2,7 @@
 # failed download with non-zero exit code
 function __sdkman_post_installation_hook {
 	echo "POST: fails with non-zero exit code"
-	echo "Can not install java 8.0.101 at this time..."
+	echo "Cannot install java 8.0.101 at this time..."
 	echo "Download has failed, aborting!"
 	return 1
 }
