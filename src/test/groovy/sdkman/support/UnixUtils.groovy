a3235954f70ef9128f8702df02f909be54c3cdcc
Ross Goldberg <ross.goldberg@gmail.com>
2020-03-10 05:47:40 -0400

    Testing: fixed macOS testing bug: UnixUtils.asSdkmanPlatform(def): added missing mapping from 'Mac OS X' to 'Darwin' other cleanup

--- src/test/groovy/sdkman/support/UnixUtils.groovy
+++ src/test/groovy/sdkman/support/UnixUtils.groovy
@@ -1,31 +1,23 @@
 package sdkman.support
 
 class UnixUtils {
-    static getPlatform() {
-        asSdkmanPlatform(System.getProperty("os.name"))
+
+    static String getPlatform() {
+        asSdkmanPlatform(System.getProperty('os.name'))
     }
 
-    static asSdkmanPlatform(platform) {
-        def result
+    static String asSdkmanPlatform(final String platform) {
         switch (platform) {
-            case "Mac OSX":
-                result = "Darwin"
-                break
-            case "Linux":
-                result = "Linux64"
-                break
-            case "Linux 64":
-                result = "Linux64"
-                break
-            case "Linux 32":
-                result = "Linux32"
-                break
-            case "FreeBSD":
-                result = "FreeBSD"
-                break
+            case 'Mac OS X':
+            case 'Mac OSX':
+                return 'Darwin'
+            case 'Linux':
+            case 'Linux 64':
+                return 'Linux64'
+            case 'Linux 32':
+                return 'Linux32'
             default:
-                result = platform
+                return platform
         }
-        result
     }
 }
