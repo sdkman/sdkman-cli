d7aef296929988df2d795d5caa0814e6403bccbe
Ross Goldberg <ross.goldberg@gmail.com>
2020-03-12 14:43:36 -0400

    Testing: HookResponses: hook script cleanup

--- src/test/groovy/sdkman/stubs/HookResponses.groovy
+++ src/test/groovy/sdkman/stubs/HookResponses.groovy
@@ -2,40 +2,44 @@ package sdkman.stubs
 
 class HookResponses {
 
-	static preInstallationHookSuccess() {
-		'''
+	static String preInstallationHookSuccess() {
+		'''\
 #!/usr/bin/env bash
 function __sdkman_pre_installation_hook {
-	echo "Pre-installation hook success"
+	echo 'Pre-installation hook success'
 	return 0
-}'''
+}
+'''
 	}
 
-	static preInstallationHookFailure() {
-		'''
+	static String preInstallationHookFailure() {
+		'''\
 #!/usr/bin/env bash
 function __sdkman_pre_installation_hook {
-	echo "Pre-installation hook failure"
+	echo 'Pre-installation hook failure'
 	return 1
-}'''
+}
+'''
 	}
 
-	static postInstallationHookSuccess() {
-		'''
+	static String postInstallationHookSuccess() {
+		'''\
 #!/usr/bin/env bash
 function __sdkman_post_installation_hook {
 	mv $binary_input $zip_output
-	echo "Post-installation hook success"
+	echo 'Post-installation hook success'
 	return 0
-}'''
+}
+'''
 	}
 
-	static postInstallationHookFailure() {
-		'''
+	static String postInstallationHookFailure() {
+		'''\
 #!/usr/bin/env bash
 function __sdkman_post_installation_hook {
-	echo "Post-installation hook failure"
+	echo 'Post-installation hook failure'
 	return 1
-}'''
+}
+'''
 	}
 }
