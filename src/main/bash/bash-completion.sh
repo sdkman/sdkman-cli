91507758dac35f986e6f03298bc09d2726a5e92c
Oliver Weiler <oliver.weiler@meinestadt.de>
2020-07-22 12:03:37 +0200

    Unquote compgen calls

--- src/main/bash/bash-completion.sh
+++ src/main/bash/bash-completion.sh
@@ -20,12 +20,12 @@ _sdk_completion() {
 	local -r previous_word="${COMP_WORDS[COMP_CWORD - 1]}"
 	local -r current_word="${COMP_WORDS[COMP_CWORD]}"
 
-	case "$previous_word" in
+ 	case "$previous_word" in
 	sdk)
-		COMPREPLY=("$(compgen -W "install uninstall list use default home env current upgrade version broadcast help offline selfupdate update flush" -- "$current_word")")
+		COMPREPLY=($(compgen -W "install uninstall list use default home env current upgrade version broadcast help offline selfupdate update flush" -- "$current_word"))
 		;;
 	env)
-		COMPREPLY=("$(compgen -W "init" -- "$current_word")")
+		COMPREPLY=($(compgen -W "init" -- "$current_word"))
 		;;
 	current)
 		local candidates=()
@@ -34,16 +34,16 @@ _sdk_completion() {
 			candidates+=("${candidate_path##*/}")
 		done
 
-		COMPREPLY=("$(compgen -W "${candidates[*]}" -- "$current_word")")
+		COMPREPLY=($(compgen -W "${candidates[*]}" -- "$current_word"))
 		;;
 	offline)
-		COMPREPLY=("$(compgen -W "enable disable" -- "$current_word")")
+		COMPREPLY=($(compgen -W "enable disable" -- "$current_word"))
 		;;
 	selfupdate)
-		COMPREPLY=("$(compgen -W "force" -- "$current_word")")
+		COMPREPLY=($(compgen -W "force" -- "$current_word"))
 		;;
 	flush)
-		COMPREPLY=("$(compgen -W "broadcast archives temp" -- "$current_word")")
+		COMPREPLY=($(compgen -W "broadcast archives temp" -- "$current_word"))
 		;;
 	esac
 }
