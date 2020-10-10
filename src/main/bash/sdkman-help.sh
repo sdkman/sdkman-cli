ec96fe835c2029cbe401ba751e0b725ec8c5137c
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-10-10 10:20:17 +0100

    Tweak help output.

--- src/main/bash/sdkman-help.sh
+++ src/main/bash/sdkman-help.sh
@@ -37,7 +37,7 @@ function __sdk_help() {
 	__sdkman_echo_no_colour "       offline           [enable|disable]"
 	__sdkman_echo_no_colour "       selfupdate        [force]"
 	__sdkman_echo_no_colour "       update"
-	__sdkman_echo_no_colour "       flush             [archives|temp|broadcast|version]"
+	__sdkman_echo_no_colour "       flush             [archives|tmp|broadcast|version]"
 	__sdkman_echo_no_colour ""
 	__sdkman_echo_no_colour "   candidate  :  the SDK to install: groovy, scala, grails, gradle, kotlin, etc."
 	__sdkman_echo_no_colour "                 use list command for comprehensive list of candidates"
