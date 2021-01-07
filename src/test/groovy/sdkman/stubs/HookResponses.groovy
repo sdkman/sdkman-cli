ca8d67c916fcdfd0b395a94cc8ac6b948b3d9d8b
Felipe Santos <felipecassiors@gmail.com>
2021-01-07 23:23:29 +0000

    Also fixes the mv commands

--- src/test/groovy/sdkman/stubs/HookResponses.groovy
+++ src/test/groovy/sdkman/stubs/HookResponses.groovy
@@ -26,7 +26,7 @@ function __sdkman_pre_installation_hook {
 		'''\
 #!/usr/bin/env bash
 function __sdkman_post_installation_hook {
-	mv $binary_input $zip_output
+	mv -f $binary_input $zip_output
 	echo "Post-installation hook success"
 	return 0
 }
--- src/test/resources/__files/hooks/post_hook_java_8.0.111_universal.sh
+++ src/test/resources/__files/hooks/post_hook_java_8.0.111_universal.sh
@@ -2,5 +2,5 @@
 # passthrough used for zip binaries
 function __sdkman_post_installation_hook() {
 	echo "POST: passthrough $binary_input to $zip_output"
-	mv "$binary_input" "$zip_output"
+	mv -f "$binary_input" "$zip_output"
 }
