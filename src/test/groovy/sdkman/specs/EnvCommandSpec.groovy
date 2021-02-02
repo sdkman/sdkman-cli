520fb0d26aa44eb996fddd1205a62ebb6917cf76
Oliver Weiler <oliver.weiler@meinestadt.de>
2021-02-02 23:20:11 +0100

    Remove multiline flag

--- src/test/groovy/sdkman/specs/EnvCommandSpec.groovy
+++ src/test/groovy/sdkman/specs/EnvCommandSpec.groovy
@@ -213,7 +213,7 @@ class EnvCommandSpec extends SdkmanEnvSpecification {
 		bash.execute("cd project")
 
 		then:
-		(bash.output =~ /(?m)Using groovy version 2.4.1 in this shell/).size() == 2
+		(bash.output =~ /Using groovy version 2.4.1 in this shell/).size() == 2
 	}
 
 	def "should execute 'sdk env clear' when exiting from a directory with an .sdkmanrc"() {
