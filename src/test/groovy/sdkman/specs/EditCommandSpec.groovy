ea1f67e950c0ea91db9a73d348041ec1f24a0781
Oliver Weiler <wo6980@gmail.com>
2021-02-16 21:58:29 +0100

    Add edit command (#874)
    * Add edit command
    * Fix failing test
    * Prevent EDITOR from being overwritten
    * Split up error message
    * Add missing test case
    
    Co-authored-by: Oliver Weiler <oliver.weiler@meinestadt.de>

---
+++ src/test/groovy/sdkman/specs/EditCommandSpec.groovy
@@ -0,0 +1,43 @@
+package sdkman.specs
+
+import sdkman.support.SdkmanEnvSpecification
+
+class EditCommandSpec extends SdkmanEnvSpecification {
+	def "it should open the config in the system's default editor"() {
+		given:
+		bash = sdkmanBashEnvBuilder
+				.withVersionCache("x.y.z")
+				.withOfflineMode(true)
+				.build()
+
+		bash.start()
+		bash.execute("source $bootstrapScript")
+
+		when:
+		setupEnv(bash)
+		bash.execute("sdk edit")
+
+		then:
+		verifyOutput(bash.output, sdkmanBaseDirectory)
+
+		where:
+		setupEnv << [
+			{
+				it.execute('nano() { echo "nano was called with $1"; }')
+				it.execute("EDITOR=nano")
+			},
+			{
+				it.execute('vi() { echo "vi was called with $1"; }')
+				it.execute("unset EDITOR")
+			},
+			{
+				it.execute("EDITOR=/does/not/exist")
+			}
+		]
+		verifyOutput << [
+			{ output, baseDirectory -> output.contains("nano was called with ${baseDirectory}/.sdkman/etc/config") },
+			{ output, baseDirectory -> output.contains("vi was called with ${baseDirectory}/.sdkman/etc/config") },
+			{ output, _ -> output.contains("No default editor configured.") }
+		]
+	}
+}
