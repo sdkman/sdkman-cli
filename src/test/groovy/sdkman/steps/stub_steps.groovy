4b3b9e0cc2516ab6c6dd89cecf934c099f6805f0
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-03-31 11:10:23 +0100

    Improve naming around UnitUtils.

--- src/test/groovy/sdkman/steps/stub_steps.groovy
+++ src/test/groovy/sdkman/steps/stub_steps.groovy
@@ -11,9 +11,9 @@ import static sdkman.support.FilesystemUtils.readVersionsCsvFromCandidateFolder
 
 And(~'^the default "([^"]*)" version is "([^"]*)"$') { String candidate, String version ->
 	primeEndpointWithString("/candidates/default/${candidate}", version)
-	primeDownloadFor(SERVICE_UP_URL, candidate, version, UnixUtils.asSdkmanPlatform())
-	primeEndpointWithString("/hooks/pre/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", preInstallationHookSuccess())
-	primeEndpointWithString("/hooks/post/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", postInstallationHookSuccess())
+	primeDownloadFor(SERVICE_UP_URL, candidate, version, UnixUtils.inferPlatform())
+	primeEndpointWithString("/hooks/pre/${candidate}/${version}/${UnixUtils.inferPlatform()}", preInstallationHookSuccess())
+	primeEndpointWithString("/hooks/post/${candidate}/${version}/${UnixUtils.inferPlatform()}", postInstallationHookSuccess())
 }
 
 And(~'^an available selfupdate$') { ->
@@ -21,67 +21,67 @@ And(~'^an available selfupdate$') { ->
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is available for download$') { String candidate, String version ->
-	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", "valid")
-	primeDownloadFor(SERVICE_UP_URL, candidate, version, UnixUtils.asSdkmanPlatform())
-	primeEndpointWithString("/hooks/pre/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", preInstallationHookSuccess())
-	primeEndpointWithString("/hooks/post/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", postInstallationHookSuccess())
+	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.inferPlatform()}", "valid")
+	primeDownloadFor(SERVICE_UP_URL, candidate, version, UnixUtils.inferPlatform())
+	primeEndpointWithString("/hooks/pre/${candidate}/${version}/${UnixUtils.inferPlatform()}", preInstallationHookSuccess())
+	primeEndpointWithString("/hooks/post/${candidate}/${version}/${UnixUtils.inferPlatform()}", postInstallationHookSuccess())
 }
 
-And(~/^the appropriate universal hooks are available for "([^"]*)" version "([^"]*)" on "([^"]*)"$/) { String candidate, String version, String platform ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform)
+And(~/^the appropriate universal hooks are available for "([^"]*)" version "([^"]*)" on "([^"]*)"$/) { String candidate, String version, String os ->
+	String lcPlatform = UnixUtils.inferPlatform(os)
 	primeUniversalHookFor("pre", candidate, version, lcPlatform)
 	primeUniversalHookFor("post", candidate, version, lcPlatform)
 }
 
-And(~/^the appropriate multi-platform hooks are available for "([^"]*)" version "([^"]*)" on "([^"]*)" with architecture "(.*)"$/) { String candidate, String version, String platform, String architecture ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform, architecture)
+And(~/^the appropriate multi-platform hooks are available for "([^"]*)" version "([^"]*)" on "([^"]*)" with architecture "(.*)"$/) { String candidate, String version, String os, String architecture ->
+	String lcPlatform = UnixUtils.inferPlatform(os, architecture)
 	primePlatformSpecificHookFor("pre", candidate, version, lcPlatform)
 	primePlatformSpecificHookFor("post", candidate, version, lcPlatform)
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is not available for download$') { String candidate, String version ->
-	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", "invalid")
+	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.inferPlatform()}", "invalid")
 }
 
-And(~/^the candidate "(.*)" version "(.*)" is available for download on "(.*)" with architecture "(.*)"$/) { String candidate, String version, String platform, String architecture ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform, architecture)
+And(~/^the candidate "(.*)" version "(.*)" is available for download on "(.*)" with architecture "(.*)"$/) { String candidate, String version, String os, String architecture ->
+	String lcPlatform = UnixUtils.inferPlatform(os, architecture)
 	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${lcPlatform}", "valid")
 	primeDownloadFor(SERVICE_UP_URL, candidate, version, lcPlatform)
 }
 
-And(~/^a "([^"]*)" install hook is served for "([^"]*)" "([^"]*)" on "([^"]*)" with architecture "([^"]*)" that returns successfully$/) { String phase, String candidate, String version, String platform, String architecture ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform, architecture)
+And(~/^a "([^"]*)" install hook is served for "([^"]*)" "([^"]*)" on "([^"]*)" with architecture "([^"]*)" that returns successfully$/) { String phase, String candidate, String version, String os, String architecture ->
+	String lcPlatform = UnixUtils.inferPlatform(os, architecture)
 	primeEndpointWithString("/hooks/${phase}/${candidate}/${version}/${lcPlatform}", phase == "pre" ? preInstallationHookSuccess() : postInstallationHookSuccess())
 }
 
-And(~/^a "([^"]*)" install hook is served for "([^"]*)" "([^"]*)" on "([^"]*)" with architecture "([^"]*)" that returns a failure$/) { String phase, String candidate, String version, String platform, String architecture ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform, architecture)
+And(~/^a "([^"]*)" install hook is served for "([^"]*)" "([^"]*)" on "([^"]*)" with architecture "([^"]*)" that returns a failure$/) { String phase, String candidate, String version, String os, String architecture ->
+	String lcPlatform = UnixUtils.inferPlatform(os, architecture)
 	primeEndpointWithString("/hooks/${phase}/${candidate}/${version}/${lcPlatform}", phase == "pre" ? preInstallationHookFailure() : postInstallationHookFailure())
 }
 
-And(~/^the candidate "(.*?)" version "(.*?)" is not available for download on "(.*?)"$/) { String candidate, String version, String platform ->
-	String lcPlatform = UnixUtils.asSdkmanPlatform(platform)
+And(~/^the candidate "(.*?)" version "(.*?)" is not available for download on "(.*?)"$/) { String candidate, String version, String os ->
+	String lcPlatform = UnixUtils.inferPlatform(os)
 	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${lcPlatform}", "invalid")
 }
 
 And(~'^a "([^"]*)" list view is available for consumption$') { String candidate ->
-	primeEndpointWithString("/candidates/${candidate}/${UnixUtils.asSdkmanPlatform()}/versions/list?current=&installed=", "Available ${candidate.capitalize()} Versions")
+	primeEndpointWithString("/candidates/${candidate}/${UnixUtils.inferPlatform()}/versions/list?current=&installed=", "Available ${candidate.capitalize()} Versions")
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is a valid candidate version$') { String candidate, String version ->
-	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", "valid")
+	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.inferPlatform()}", "valid")
 }
 
 And(~'^the candidate "([^"]*)" version "([^"]*)" is not a valid candidate version$') { String candidate, String version ->
-	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.asSdkmanPlatform()}", "invalid")
+	primeEndpointWithString("/candidates/validate/${candidate}/${version}/${UnixUtils.inferPlatform()}", "invalid")
 }
 
 And(~/^the candidate "(.*?)" has a version list available$/) { String candidate ->
 	def current = readCurrentFromCandidateFolder(candidatesDir, candidate)
 	def versions = readVersionsCsvFromCandidateFolder(candidatesDir, candidate)
-	def url = "/candidates/${candidate}/${UnixUtils.asSdkmanPlatform()}/versions/list?current=${current}&installed=${versions}"
+	def url = "/candidates/${candidate}/${UnixUtils.inferPlatform()}/versions/list?current=${current}&installed=${versions}"
 	println("Priming url: $url")
-	primeEndpointWithString(url, "Candidate: $candidate; Versions: $versions; Current: $current; Platform: ${UnixUtils.asSdkmanPlatform()}")
+	primeEndpointWithString(url, "Candidate: $candidate; Versions: $versions; Current: $current; Platform: ${UnixUtils.inferPlatform()}")
 }
 
 And(~/^The candidate list is available$/) { ->
