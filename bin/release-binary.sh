6d7092120dc48204c854a50cce7b344ab76c74b2
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-06-21 13:50:24 +0100

    Further improve propagation of secrets to mongo helper in beta action.

--- bin/release-binary.sh
+++ bin/release-binary.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 BRANCH="$1"
-BUILD_NUMBER="$2"
+SHORT_HASH="$2"
 MONGO_URL="$3"
 MONGO_USERNAME="$4"
 MONGO_PASSWORD="$5"
-VERSION="$BRANCH+$BUILD_NUMBER"
+VERSION="$BRANCH+$SHORT_HASH"
 
 if [[ -z "$MONGO_USERNAME" || -z "$MONGO_PASSWORD" ]]; then
 	echo "No mongo credentials so doing nothing..."
-	return 0
+	return 1
 fi
 
 echo "Mongo URL: $MONGO_URL"
@@ -20,6 +20,6 @@ else
 	FIELD="stableCliVersion"
 fi
 
-echo "Release: $FIELD $VERSION"
+echo "Release: $FIELD as $VERSION"
 
 mongo "${MONGO_URL}" --username="${MONGO_USERNAME}" --password="${MONGO_PASSWORD}" -eval "db.application.updateOne({}, {\$set: { \"$FIELD\": \"$VERSION\"}});"
