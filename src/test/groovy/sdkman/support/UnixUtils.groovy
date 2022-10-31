a361741c57b2bca51cba55092de3e2ec790083db
Hector Geraldino <hgeraldino@gmail.com>
2022-10-31 12:17:12 -0400

    Fix test failures in OSX/Intel when inferring the platform

--- src/test/groovy/sdkman/support/UnixUtils.groovy
+++ src/test/groovy/sdkman/support/UnixUtils.groovy
@@ -6,11 +6,9 @@ class UnixUtils {
 			"Linux"   : [
 					"x86_64": "LinuxX64"
 			],
-			"Mac OS X": [
-					"x86_64": "DarwinX64",
-			],
 			"Darwin": [
 					"x86_64": "DarwinX64",
+					"arm64": "DarwinX64",
 			]
 	]
 
