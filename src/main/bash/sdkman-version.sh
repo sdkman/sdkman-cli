155e6f87620531450ec5d8956560038d843e8975
Edwin Kofler <edwin@kofler.dev>
2023-10-07 03:56:04 -0700

    fix: Quote `$SDKMAN_DIR` in version subcommand

--- src/main/bash/sdkman-version.sh
+++ src/main/bash/sdkman-version.sh
@@ -17,7 +17,7 @@
 #
 
 function __sdk_version() {
-    local version=$(cat $SDKMAN_DIR/var/version)
+	local version=$(cat "$SDKMAN_DIR/var/version")
 	echo ""
 	__sdkman_echo_yellow "SDKMAN $version"
 }
